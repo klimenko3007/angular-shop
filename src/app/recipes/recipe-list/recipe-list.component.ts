b3def595d37b2519537ae21eb7344162b7564e1c
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-12-06 20:54:27 +0100

    Recipe detail template complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,7 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is a simple example recipe',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
-    )
+    ),
   ];
 
   constructor() {}
