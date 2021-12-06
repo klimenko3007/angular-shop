b376ed763026e57a4552fbc6b9b1c0c2ea3b5483
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-12-06 21:20:28 +0100

    Shopping list and shopping edit templates complete

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
-
+import { Ingredient } from "../shared/ingredient.model";
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('bananas', 6)
+
+  ];
+  constructor() {}
   
 }
