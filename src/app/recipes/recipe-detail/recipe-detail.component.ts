0d2d15ca4639b1252e61ddc27b958a309bd14e7a
klimenko3007 <ek1830@tobii.com>
2022-01-03 17:09:51 +0100

    Refactored addShopping list to fire less events

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Recipe } from "@recipes/recipe.model";
-import { Ingredient } from "@shared/ingredient.model";
+//import { Ingredient } from "@shared/ingredient.model";
 import { ShoppingListService } from "src/app/shopping-list/shopping-list.service";
 @Component({
   selector: 'app-recipe-detail',
@@ -11,9 +11,8 @@ export class RecipeDetailComponent {
   constructor( private slService: ShoppingListService) {}
   
   addToShoppingList(){
-    this.recipe.ingredients.forEach((ingredient: Ingredient) => {
-      this.slService.addIngredient(ingredient);
-    });
+    this.slService.ingredients.push(...this.recipe.ingredients);
+    this.slService.changedIngredients.emit(this.slService.ingredients.slice());
   }
 
 } 
