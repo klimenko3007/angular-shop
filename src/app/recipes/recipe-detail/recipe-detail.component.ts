fe143ecea8f7336b27b63ad14bf121624f884194
klimenko3007 <ek1830@tobii.com>
2022-01-03 16:45:35 +0100

    Recipe detail component has ingedients and functioning add to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from "@angular/core";
 import { Recipe } from "@recipes/recipe.model";
+import { Ingredient } from "@shared/ingredient.model";
+import { ShoppingListService } from "src/app/shopping-list/shopping-list.service";
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+  constructor( private slService: ShoppingListService) {}
+  
+  addToShoppingList(){
+    this.recipe.ingredients.forEach((ingredient: Ingredient) => {
+      this.slService.addIngredient(ingredient);
+    });
+  }
 
 } 
