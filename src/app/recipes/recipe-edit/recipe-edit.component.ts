9bed066789d7de3b8ecb346134b939e8dab7bf72
klimenko3007 <ek1830@tobii.com>
2022-04-28 11:39:19 +0200

    Recipe edit component works with form group and takes values from the recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '@recipes/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +11,54 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
   
-  constructor( private route: ActivatedRoute) { }
+  constructor( private route: ActivatedRoute, private recipeService: RecipeService) { }
+
+  formInit () {
+    const editedRecipe = {
+      name: '',
+      description: '',
+      imagePath: '',
+      ingredients: new FormArray([])
+    };
+    
+    if(this.editMode) {
+      const recipe = this.recipeService.findRecipeById(this.id);
+      editedRecipe.name = recipe.name; 
+      editedRecipe.description = recipe.description, 
+      editedRecipe.imagePath= recipe.imagePath;
+
+      if(recipe.ingredients) {
+        recipe.ingredients.forEach((ingredient) => {
+          editedRecipe.ingredients.push( new FormGroup({
+            name: new FormControl(ingredient.name),
+            amount: new FormControl(ingredient.amount)
+          }));
+        });
+      }
+    }
+    
+    this.recipeForm = new FormGroup ({
+      name: new FormControl(editedRecipe.name),
+      imagePath: new FormControl(editedRecipe.imagePath),
+      description: new FormControl(editedRecipe.description),
+      ingredients: editedRecipe.ingredients
+    
+    });
+    console.log(this.recipeForm);
+  }
+
+  get ingredientsArray() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+   }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params.id;
         this.editMode = params.id != null; // checking whether there is params if there are none we are in new recipe mode
+        this.formInit();
       }
     );
 
